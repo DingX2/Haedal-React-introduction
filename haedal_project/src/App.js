7e3fd69ec703418d9b7f19107b451c0abb2ff869
DingX2 <pourding@naver.com>
2023-02-05 10:35:37 +0900

    LSH2

--- haedal_project/src/App.js
+++ haedal_project/src/App.js
@@ -4,10 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Learn
-      </header>
+      <header className="App-header">이수현 수정중... </header>
     </div>
   );
 }
