c401f6de6d26654d03858d5c765101aab490cd10
WhalesBob <ndw8200@naver.com>
2023-02-12 23:25:14 +0900

    feat : 메인 페이지 구성(with React Swiper ver 8.5.5)

--- haedal_project/src/App.js
+++ haedal_project/src/App.js
@@ -1,13 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import MainPage from "./MainPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">이수현 수정중... </header>
-      <header className="App-header">남동우 수정중... </header>
-      <header className="App-header">김현주 수정중... </header>
-      <header className="App-header">조현호 수정중... </header>
+      <MainPage></MainPage>
     </div>
   );
 }
