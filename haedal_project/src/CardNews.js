2f3397ccc45cb00930517d63fdd79b9b0cbb5fe1
DingX2 <pourding@naver.com>
2023-02-14 00:42:14 +0900

    CardNews_emla_sucess

--- haedal_project/src/CardNews.js
+++ haedal_project/src/CardNews.js
@@ -2,18 +2,38 @@ import "./CardNews.css";
 import React from "react";
 
 //npm i react react-dom @trendyol-js/react-carousel --save
-import ReactDOM from "react-dom";
-import { Carousel } from "@trendyol-js/react-carousel";
-import { Item } from "./YourItem";
+import { useEffect } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 
 export default function CardNews() {
-  {
-    return ReactDOM.render(
-      <Carousel>
-        <img src={"/img/카드들.png"} className="image-wrap" />
-        <img src={"/img/카드들.png"} className="image-wrap" />
-      </Carousel>,
-      document.getElementById("root")
-    );
-  }
+  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
+
+  const autoplayOptions = {
+    delay: 10,
+    rootNode: (emblaRoot) => emblaRoot.parentElement,
+  };
+
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        <img src={"/img/카드들.png"} className="embla__slide" alt="" />
+        <img src={"/img/카드들2.png"} className="embla__slide" alt="" />
+      </div>
+    </div>
+  );
 }
+
+/*
+        <img src={"/img/카드들.png"} className="embla__slide" />
+        <img src={"/img/카드들2.png"} className="embla__slide" />
+
+export default function CardNews() {
+  return ReactDOM.render(
+    <Carousel>
+      <img src={"/img/카드들.png"} className="image-wrap" />
+      <img src={"/img/카드들2.png"} className="image-wrap" />
+    </Carousel>,
+    document.getElementById("root")
+  );
+}*/
