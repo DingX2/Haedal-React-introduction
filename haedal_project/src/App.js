91c0ec0a004042769c50187d11af718807b6d2b0
WhalesBob <ndw8200@naver.com>
2023-02-14 21:19:32 +0900

    [FIX] 버전 관리 및 통합 뷰 생성

--- haedal_project/src/App.js
+++ haedal_project/src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
-import MainPage from "./MainPage";
+import Banner from "./Banner.js";
+import MainPage from "./MainPage.js";
+import CardNews from "./CardNews";
 
 function App() {
   return (
     <div className="App">
-      <MainPage></MainPage>
+
+      <Banner />
+      {/*아래는 네비게이션바*/}
+      {/* 메인페이지 슬라이더*/}
+      <br/><br/><br/>
+      <MainPage/>
+      {/* 메인페이지*/}
+      {/* 캘린더*/}
+      {/* 공지*/}
+      {/* 카드뉴스*/}
+      <div>카드뉴스</div>
+      <CardNews />
+
+      {/*  버튼    */}
+      {/* 지원하기*/}
+      {/* 하단 상세페이지*/}
     </div>
   );
 }
