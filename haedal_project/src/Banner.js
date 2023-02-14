1cfa406e8c0f51f26340de4124efb46b043edc23
hyunjoooo <m3njooki@gmail.com>
2023-02-14 19:48:47 +0900

    first banner commit

--- haedal_project/src/Banner.js
+++ haedal_project/src/Banner.js
@@ -1,17 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import './Banner.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="black-nav">
+function Banner () { return (
+    <div className="Banner">
+    <div className="black-nav">
         <h4 className="button">HAEDAL</h4>
         <h4 className="button">메뉴1</h4>
         <h4 className="button">메뉴2</h4>
         <h4 className="button">메뉴3</h4>
     </div>
     </div>
-  );
-}
+)}
 
-export default App;
+export default Banner;
