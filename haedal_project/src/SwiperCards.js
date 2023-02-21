13fd940e1085c49998ed58ad1b52ab69df76df26
Hyeonho <hyeonhojo919@gmail.com>
2023-02-21 19:01:42 +0900

    cardnews title added

--- haedal_project/src/SwiperCards.js
+++ haedal_project/src/SwiperCards.js
@@ -19,25 +19,36 @@ function SwiperCards() {
     "./images/card5.png",
   ];
   return (
-    <Swiper
-      className='SwiperCards'
-      spaceBetween={30}
-      slidesPerView="auto"
-      autoplay={{
-        delay: 0,
-        disableOnInteraction: false, // 클릭 후 자동재생 비활성화 방지
-      }}
-      loop={true} // 무한 슬라이드
-      speed={2000} // 높을수록 느림
-      freeMode={false}
-      loopAdditionalSlides={9}
-    >
-      {images.map((img, index) => (
-        <SwiperSlide className='SwiperCards'>
-          <img src={img} alt="데굴 데굴 데굴 데굴" style={{background: "gray", height: '200px', width: '200px'}}/>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className='cardnews_body'>
+      <div className='cardnews_title'>
+        <div className='cardnews_title_big'>
+          <span className='cardnews_title_quote_open'>“</span>
+          <span>오 누가 이걸 몰라?!</span>
+          <span className='cardnews_title_quote_close'>”</span>
+        </div>
+        <div className='cardnews_title_small'>
+          <span>해달 오누이 IT 카드뉴스</span>
+        </div>
+      </div>
+      <Swiper
+        spaceBetween={30}
+        slidesPerView="auto"
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false, // 클릭 후 자동재생 비활성화 방지
+        }}
+        loop={true} // 무한 슬라이드
+        speed={3000} // 높을수록 느림
+        freeMode={false}
+        loopAdditionalSlides={9}
+      >
+        {images.map((img, index) => (
+          <SwiperSlide className='SwiperCards'>
+            <img src={img} alt="img" style={{background: "gray", height: '200px', width: '200px'}}/>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
