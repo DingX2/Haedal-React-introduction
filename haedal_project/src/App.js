9898acdc7831d30d419bac458e890ce8b2996cbb
WhalesBob <ndw8200@naver.com>
2023-02-23 16:22:03 +0900

    [FEAT] 버튼 클릭 component 부분 완성

--- haedal_project/src/App.js
+++ haedal_project/src/App.js
@@ -6,6 +6,7 @@ import Calender from "./Calender";
 import Footer from "./Footer";
 import CardNews from "./CardNews";
 import Apply from "./Apply";
+import Buttons from "./Buttons";
 
 const OPTIONS = {};
 const SLIDE_COUNT = 5;
@@ -14,7 +15,7 @@ const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 function App() {
   return (
     <div className="App">
-      <Banner />
+      <Banner />s
       {/*아래는 네비게이션바*/}
       {/* 메인페이지 슬라이더*/}
       <br />
@@ -27,6 +28,7 @@ function App() {
       {/* 카드뉴스*/}
       <CardNews />
       {/*  버튼    */}
+      <Buttons/>
       {/* 지원하기*/}
       <Apply />
       {/* 하단 상세페이지*/}
---
+++ haedal_project/src/OneButton.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./css/OneButton.css";
+
+const OneButton = (props) => (
+  <>
+    <a href={`${props.url}`}>
+        <div 
+        id={props.id} 
+        className="OneButton" 
+        style={{ 
+            backgroundImage: `url(${props.image})`,
+            backgroundSize: "cover"
+        }}>
+            <div className="contentText">
+                <p> {props.content} </p>
+            </div>
+        </div>
+    </a>
+  </>
+);
+
+export default OneButton;
