ac3a62414c3fcc5d507647b792f9c06019abe046
Hyeonho <hyeonhojo919@gmail.com>
2023-02-24 17:13:14 +0900

    카드뉴스 확대 기능 추가, swiper css 충돌 수정

--- haedal_project/src/CardNews.js
+++ haedal_project/src/CardNews.js
@@ -31,55 +31,53 @@ function CardNews() {
   SwiperCore.use([Autoplay]);
 
   return (
-    <div className="swiper-view">
-      <div className="cardnews_body">
-        <div className="cardnews_title">
-          <div className="cardnews_title_big">
-            <span className="cardnews_title_quote_open">“</span>
-            <span>오 누가 이걸 몰라?!</span>
-            <span className="cardnews_title_quote_close">”</span>
-          </div>
-          <div className="cardnews_title_small">
-            <span>해달 오누이 IT 카드뉴스</span>
-          </div>
+    <div className="cardnews_body">
+      <div className="cardnews_title">
+        <div className="cardnews_title_big">
+          <span className="cardnews_title_quote_open">“</span>
+          <span>오 누가 이걸 몰라?!</span>
+          <span className="cardnews_title_quote_close">”</span>
         </div>
-        <div // onMouseEnter, onMouseLeave 넣기 위한 div>
-          onMouseEnter={() => {
-            setMouseEnter(true);
-            swiperRef.current.swiper.autoplay.stop();
+        <div className="cardnews_title_small">
+          <span>해달 오누이 IT 카드뉴스</span>
+        </div>
+      </div>
+      <div // onMouseEnter, onMouseLeave 넣기 위한 div>
+        onMouseEnter={() => {
+          setMouseEnter(true);
+          swiperRef.current.swiper.autoplay.stop();
+        }}
+        onMouseLeave={() => {
+          setMouseEnter(false);
+          swiperRef.current.swiper.autoplay.start();
+          setTimeout(() => {
+            if (mouseEnter === false) {
+              swiperRef.current.swiper.autoplay.start();
+            }
+          }, speed); // 일정 시간 뒤 마우스가 swiper 위에 없으면 autoplay 재시작
+        }}
+      >
+        <Swiper
+          ref={swiperRef}
+          spaceBetween={25}
+          slidesPerView="auto"
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false, // 클릭 후 자동재생 비활성화 방지
           }}
-          onMouseLeave={() => {
-            setMouseEnter(false);
-            swiperRef.current.swiper.autoplay.start();
-            setTimeout(() => {
-              if (mouseEnter === false) {
-                swiperRef.current.swiper.autoplay.start();
-              }
-            }, speed); // 일정 시간 뒤 마우스가 swiper 위에 없으면 autoplay 재시작
-          }}
+          loop={true} // 무한 슬라이드
+          speed={speed} // 카드 하나당 시간
+          loopedSlides={3}
+          centeredSlides // 가운데 시작
+          grabCursor // 잡기 가능
+          pauseOnMouseEnter={true}
         >
-          <Swiper
-            ref={swiperRef}
-            spaceBetween={30}
-            slidesPerView="auto"
-            autoplay={{
-              delay: 0,
-              disableOnInteraction: false, // 클릭 후 자동재생 비활성화 방지
-            }}
-            loop={true} // 무한 슬라이드
-            speed={speed} // 카드 하나당 시간
-            loopedSlides={3}
-            centeredSlides // 가운데 시작
-            grabCursor // 잡기 가능
-            pauseOnMouseEnter={true}
-          >
-            {images.map((img, index) => (
-              <SwiperSlide>
-                <img className="cards" src={img} alt={`card${index}`} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
+          {images.map((img, index) => (
+            <SwiperSlide>
+              <img className="cards" src={img} alt={`card${index}`} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
