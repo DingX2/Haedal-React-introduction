114a4aa554b605f6693bb080b332fad05b21c893
Hyeonho <hyeonhojo919@gmail.com>
2023-02-27 13:05:17 +0900

    지원하기 애니메이션

--- haedal_project/src/Calender.js
+++ haedal_project/src/Calender.js
@@ -4,7 +4,7 @@ function Calender() {
   return (
     <div className="Calender">
       <div className="Calender_view">
-        <img className="Calender" alt="Calender" src="./img/calender.png" />
+        <img className="Calender_image" alt="Calender" src="./img/calender.png" />
       </div>
     </div>
   );
